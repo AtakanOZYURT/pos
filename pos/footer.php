683d75fd688286f54e0b3c75deb97ceefc41448c
AtakanOZYURT <atakan.ozyurt96@gmail.com>
2024-07-03 16:45:59 +0300

    Delete pos directory

--- pos/footer.php
+++
@@ -1,6 +0,0 @@
-<footer>
-        <p>&copy; 2024 Hızlı Satış. Tüm hakları saklıdır.</p>
-    </footer>
-    <link rel="stylesheet" href="css/footer.css">
-</body>
-</html>
--- pos/index.php
+++
@@ -1,7 +0,0 @@
-<?php include 'header.php'; ?>
-<link rel="stylesheet" type="text/css" href="css/index.css">
-<main>
-    <h1>Hoşgeldiniz</h1>
-    <p>Anasayfa içeriği buraya gelecek.</p>
-</main>
-<?php include 'footer.php'; ?>
